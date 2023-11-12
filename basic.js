9ae66d8324b9d63b020d5e64129b721a2946a280
Annie <annietang.california@gmail.com>
2023-11-11 22:05:30 -0800

    first commit

---
+++ basic.js
@@ -0,0 +1,14 @@
+console.log("hi")
+var userName = "me"
+function printUserName(param) {
+    return param
+}
+
+console.log(printUserName(userName))
+console.log(printUserName("Annie"))
+
+var UserName = "Joe"  //identifier are case sensitive
+console.log(userName)
+console.log(UserName)
+
+//combiled as well as interpreted language due to hoisting present in javascript
